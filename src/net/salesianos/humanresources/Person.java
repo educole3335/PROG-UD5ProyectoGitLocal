91e35f0fd9db494fff230f7e94b81c5c6ec83170
eduardogmunoz <usr3335@salesianos-lacuesta.net>
2025-04-29 09:56:22 +0100

    feat(Person.java): Clase person y sus paquetes

---
+++ src/net/salesianos/humanresources/Person.java
@@ -0,0 +1,12 @@
+package net.salesianos.humanresources;
+
+public class Person {
+
+    private String name;
+    private String arge;
+    
+    public Person(String name, String arge) {
+        this.name = name;
+        this.arge = arge;
+    }
+}
